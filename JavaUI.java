f243762dc9d4b77956070f6bd70e0b592ea35ff9
Optio1 <Agentoptio@gmail.com>
2022-11-03 19:13:19 -0500

    Create JavaUI.java
    Deklan Glaser Nov 3 2022

---
+++ JavaUI.java
@@ -0,0 +1,16 @@
+package packag;
+//this will not execute due to "main does not exist"
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class JavaUI {
+
+	public static void main(String[] args) {
+		JFrame frame =new JFrame();
+		JButton b=new JButton("click");
+		frame.setBounds(130,100,100, 40);  
+	
+
+	}
+
+}
